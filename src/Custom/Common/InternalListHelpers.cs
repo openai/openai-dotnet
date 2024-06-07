04d479fe8cc9260f526933e40063c4f35f6b5c5d
HavenDV <havendv@gmail.com>
2024-06-07 11:09:02 +0400

    fix: Added DynamicallyAccessedMembers to InternalListHelpers.

--- src/Custom/Common/InternalListHelpers.cs
+++ src/Custom/Common/InternalListHelpers.cs
@@ -1,5 +1,6 @@
 using System.ClientModel;
 using System.ClientModel.Primitives;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -10,28 +11,40 @@ internal static class InternalListHelpers
     internal delegate Task<ClientResult> AsyncListResponseFunc(string continuationToken, int? pageSize);
     internal delegate ClientResult ListResponseFunc(string continuationToken, int? pageSize);
 
-    internal static AsyncPageableCollection<T> CreateAsyncPageable<T, U>(AsyncListResponseFunc listResponseFunc)
-        where U : IJsonModel<U>, IInternalListResponse<T>
+    internal static AsyncPageableCollection<T> CreateAsyncPageable<T,
+#if NET6_0_OR_GREATER
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+#endif
+        TInternalList>(AsyncListResponseFunc listResponseFunc)
+        where TInternalList : IJsonModel<TInternalList>, IInternalListResponse<T>
     {
         async Task<ResultPage<T>> pageFunc(string continuationToken, int? pageSize)
-            => GetPageFromProtocol<T,U>(await listResponseFunc(continuationToken, pageSize).ConfigureAwait(false));
+            => GetPageFromProtocol<T,TInternalList>(await listResponseFunc(continuationToken, pageSize).ConfigureAwait(false));
         return PageableResultHelpers.Create((pageSize) => pageFunc(null, pageSize), pageFunc);
     }
 
-    internal static PageableCollection<T> CreatePageable<T, U>(ListResponseFunc listResponseFunc)
-        where U : IJsonModel<U>, IInternalListResponse<T>
+    internal static PageableCollection<T> CreatePageable<T,
+#if NET6_0_OR_GREATER
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+#endif
+        TInternalList>(ListResponseFunc listResponseFunc)
+        where TInternalList : IJsonModel<TInternalList>, IInternalListResponse<T>
     {
         ResultPage<T> pageFunc(string continuationToken, int? pageSize)
-            => GetPageFromProtocol<T, U>(listResponseFunc(continuationToken, pageSize));
+            => GetPageFromProtocol<T, TInternalList>(listResponseFunc(continuationToken, pageSize));
         return PageableResultHelpers.Create((pageSize) => pageFunc(null, pageSize), pageFunc);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ResultPage<TInstance> GetPageFromProtocol<TInstance, UInternalList>(ClientResult protocolResult)
-            where UInternalList : IJsonModel<UInternalList>, IInternalListResponse<TInstance>
+    private static ResultPage<TInstance> GetPageFromProtocol<TInstance,
+#if NET6_0_OR_GREATER
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+#endif
+        TInternalList>(ClientResult protocolResult)
+            where TInternalList : IJsonModel<TInternalList>, IInternalListResponse<TInstance>
     {
         PipelineResponse response = protocolResult.GetRawResponse();
-        IInternalListResponse<TInstance> values = ModelReaderWriter.Read<UInternalList>(response.Content);
+        IInternalListResponse<TInstance> values = ModelReaderWriter.Read<TInternalList>(response.Content);
         return ResultPage<TInstance>.Create(values.Data, values.HasMore ? values.LastId : null, response);
     }
 }
