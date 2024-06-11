2fb9c9c4b5eec1838f91a8319c1aa786cd80137c
Stephen Toub <stoub@microsoft.com>
2024-06-11 16:40:40 -0400

    Address PR feedback

--- src/Utility/MultipartFormDataBinaryContent.cs
+++ src/Utility/MultipartFormDataBinaryContent.cs
@@ -120,7 +120,7 @@ internal class MultipartFormDataBinaryContent : BinaryContent
         content.Headers.ContentType = header;
     }
 
-#if NET
+#if NET6_0_OR_GREATER
     private static string CreateBoundary() =>
         string.Create(BoundaryLength, 0, (chars, _) =>
         {
@@ -145,14 +145,12 @@ internal class MultipartFormDataBinaryContent : BinaryContent
             _random.NextBytes(random);
         }
 
-        // The following will sample evenly from the possible values.
-        // This is important to ensuring that the odds of creating a boundary
-        // that occurs in any content part are astronomically small.
+        // Instead of `% BoundaryValues.Length` as is used above, use a mask to achieve the same result.
+        // `% BoundaryValues.Length` is optimized to the equivalent on .NET Core but not on .NET Framework.
         const int Mask = 255 >> 2;
-
         Debug.Assert(BoundaryValues.Length - 1 == Mask);
 
-        for (int i = 0; i < random.Length; i++)
+        for (int i = 0; i < chars.Length; i++)
         {
             chars[i] = BoundaryValues[random[i] & Mask];
         }
@@ -177,19 +175,18 @@ internal class MultipartFormDataBinaryContent : BinaryContent
 
     public override void WriteTo(Stream stream, CancellationToken cancellationToken = default)
     {
-        // TODO: polyfill sync-over-async for netstandard2.0 for Azure clients.
-        // Tracked by https://github.com/Azure/azure-sdk-for-net/issues/42674
-
-#if NET
+#if NET5_0_OR_GREATER
         _multipartContent.CopyTo(stream, default, cancellationToken);
 #else
+        // TODO: polyfill sync-over-async for netstandard2.0 for Azure clients.
+        // Tracked by https://github.com/Azure/azure-sdk-for-net/issues/42674
         _multipartContent.CopyToAsync(stream).GetAwaiter().GetResult();
 #endif
     }
 
     public override async Task WriteToAsync(Stream stream, CancellationToken cancellationToken = default)
     {
-#if NET
+#if NET5_0_OR_GREATER
         await _multipartContent.CopyToAsync(stream, cancellationToken).ConfigureAwait(false);
 #else
         await _multipartContent.CopyToAsync(stream).ConfigureAwait(false);
