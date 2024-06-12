77f03a339b77b529cf0f6087276c81d69d7714f9
Krzysztof Cwalina <kcwalina@microsoft.com>
2024-06-11 17:17:42 -0700

    Added sample showing how to do cancellations

---
+++ examples/Chat/Example01_SimpleChat_Cancellations.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenAI.Chat;
+using System;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+using System.Threading;
+
+namespace OpenAI.Examples;
+
+public partial class ChatExamples
+{
+    [Test]
+    public void Example01_SimpleChat_Cancellations()
+    {
+        ChatClient client = new(model: "gpt-4o", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+        CancellationTokenSource ct = new CancellationTokenSource();
+        RequestOptions options = new() { CancellationToken = ct.Token };
+
+        // The following code will be simplified in the future.
+        var wireFormat = new ModelReaderWriterOptions("W");
+        ChatMessage message = ChatMessage.CreateUserMessage("Say 'this is a test.'");
+        BinaryData json = ModelReaderWriter.Write(message, wireFormat);
+
+        ClientResult result = client.CompleteChat(BinaryContent.Create(json), options);
+        
+        ChatCompletion completion = ModelReaderWriter.Read<ChatCompletion>(result.GetRawResponse().Content, wireFormat);
+        Console.WriteLine($"[ASSISTANT]: {completion}");
+    }
+}
