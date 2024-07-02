0610ab4672679983c8018bb41289957dbca289b9
Liudmila Molkova <limolkova@microsoft.com>
2024-07-02 14:08:47 -0700

    Add instrumentation for convenience chat calls without stremaing

---
+++ src/Custom/Common/Instrumentation/InstrumentationFactory.cs
@@ -0,0 +1,25 @@
+﻿using OpenAI.Chat;
+using System;
+
+namespace OpenAI.Custom.Common.Instrumentation;
+
+internal class InstrumentationFactory
+{
+    private readonly string _serverAddress;
+    private readonly int _serverPort;
+    private readonly string _model;
+
+    private const string ChatOperationName = "chat";
+
+    public InstrumentationFactory(string model, Uri endpoint)
+    {
+        _serverAddress = endpoint.Host;
+        _serverPort = endpoint.Port;
+        _model = model;
+    }
+
+    public InstrumentationScope StartChatScope(ChatCompletionOptions completionsOptions)
+    {
+        return InstrumentationScope.StartChat(_model, ChatOperationName, _serverAddress, _serverPort, completionsOptions);
+    }
+}
