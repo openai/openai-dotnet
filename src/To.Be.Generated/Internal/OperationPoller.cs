fc5d7ed1141079d7d6c0b7ef901c797970bc54a6
Anne Thompson <annelo@microsoft.com>
2024-07-05 16:28:34 -0700

    add missed files

---
+++ src/To.Be.Generated/Internal/OperationPoller.cs
@@ -0,0 +1,19 @@
+﻿using System.ClientModel;
+
+#nullable enable
+
+namespace OpenAI;
+
+// Convenience version
+// Note: Right now, this just inherits from the protocol poller and adds a T.
+// Do we need more than that?
+internal abstract class OperationPoller<T> : OperationResultPoller
+{
+    protected OperationPoller(ClientResult current) : base(current)
+    {
+    }
+
+    public ClientResult<T> Value => GetValueFromResult(Current);
+
+    public abstract ClientResult<T> GetValueFromResult(ClientResult result);
+}
---
+++ src/To.Be.Generated/Internal/OperationResultPoller.cs
@@ -0,0 +1,60 @@
+﻿using System.ClientModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace OpenAI;
+
+// Protocol version
+// Note: idea here is that this type is generated to be specific to the client.
+// Like PageResultEnumerator, the subtype will be generated and wrapped in an
+// outer public type.
+internal abstract class OperationResultPoller
+{
+    private const int DefaultWaitMilliseconds = 1000;
+
+    protected OperationResultPoller(ClientResult current)
+    {
+        Current = current;
+    }
+
+    // TODO: Thread-safe assignment?
+    public ClientResult Current { get; protected set; }
+
+    // Service-specific methods to be generated on the subclient
+    public abstract Task<ClientResult> UpdateStatusAsync();
+
+    public abstract ClientResult UpdateStatus();
+
+    public abstract bool HasStopped(ClientResult result);
+
+    // TODO: how does RequestOptions/CancellationToken work?
+    public async Task WaitForCompletionAsync()
+    {
+        bool hasStopped = HasStopped(Current);
+
+        while (!hasStopped)
+        {
+            // TODO: implement an interesting wait routine
+            await Task.Delay(DefaultWaitMilliseconds);
+
+            Current = await UpdateStatusAsync().ConfigureAwait(false);
+            hasStopped = HasStopped(Current);
+        }
+    }
+
+    public void WaitForCompletion()
+    {
+        bool hasStopped = HasStopped(Current);
+
+        while (!hasStopped)
+        {
+            // TODO: implement an interesting wait routine
+            Thread.Sleep(DefaultWaitMilliseconds);
+
+            Current = UpdateStatus();
+            hasStopped = HasStopped(Current);
+        }
+    }
+}
