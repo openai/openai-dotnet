eb94c20d9175f2edfc246a9ce6c72269cfc879e2
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-06-26 14:56:57 -0700

    changes s3 to use system message

--- docs/examples/text-and-prompting/s3-chat_instructions.cs
+++ docs/examples/text-and-prompting/s3-chat_instructions.cs
@@ -7,7 +7,7 @@ using OpenAI.Chat;
 string key = Environment.GetEnvironmentVariable("OPENAI_KEY")!;
 ChatClient client = new("gpt-4.1", key);
 ChatCompletion acompletion = client.CompleteChat([
-    ChatMessage.CreateDeveloperMessage("Talk like a pirate."),
+    ChatMessage.CreateSystemMessage("Talk like a pirate."),
     ChatMessage.CreateUserMessage("Are semicolons optional in JavaScript?")
 ]);
 Console.WriteLine(acompletion.Content[0].Text);
