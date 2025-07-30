50e4660dc5eab3ee90948c0ddaa35bd4a340e0e7
Arun Chandrasekhar <14188700+achandmsft@users.noreply.github.com>
2025-07-30 19:53:40 +0530

    Create responses_generatetext.cs

---
+++ docs/quickstart/responses/responses_generatetext.cs
@@ -0,0 +1,12 @@
+// SAMPLE: Developer QuickStart
+// GUIDANCE: https://platform.openai.com/docs/overview 
+// HOWTO: Instructions to run this code: https://aka.ms/oai/net/start 
+#:package OpenAI@2.2.*-*
+#:property PublishAot false
+
+using OpenAI.Responses; 
+
+string key = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
+OpenAIResponseClient client = new("gpt-4.1", key);
+OpenAIResponse response = client.CreateResponse("Write a one-sentence bedtime story about a unicorn.");
+Console.WriteLine(response.GetOutputText());
