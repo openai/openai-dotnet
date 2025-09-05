57c7164adcd7a334314635c8d77be6b31b5056dd
Jose Arriaga Maldonado <45773732+joseharriaga@users.noreply.github.com>
2025-09-05 13:45:46 -0700

    Refactor `Start*Session` methods in `RealtimeClient` (#658)
    Replaced the `RequestOptions` parameter for a new `RealtimeSessionOptions` parameter and a `CancellationToken` parameter.

---
+++ src/Custom/Realtime/RealtimeSessionOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OpenAI.Realtime;
+
+[Experimental("OPENAI002")]
+public class RealtimeSessionOptions
+{
+    public RealtimeSessionOptions()
+    {
+        Headers = new ChangeTrackingDictionary<string, string>();
+    }
+
+    public string QueryString { get; set; }
+
+    public IDictionary<string, string> Headers { get; }
+}
