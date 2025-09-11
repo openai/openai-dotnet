81c479521f39b079ad7e9b7ddf640172c8813941
Justin Yoo <justin.yoojh@gmail.com>
2025-09-12 06:55:24 +0900

    Update file-based apps to be aligned with .NET 10 RC1 (#681)
    This PR is mainly for fixing two things:
    
    - Updated the `#:property` directive to be working with .NET 10 RC1.
    - Updated the OpenAI SDK package to use `2.*` instead of `2.2.*-*`.
    - Added the `OPENAI001` warning suppressions for some methods.

--- docs/guides/text/responses/responses_prompttemplate.cs
+++ docs/guides/text/responses/responses_prompttemplate.cs
@@ -1,7 +1,9 @@
 // SAMPLE: Generate text with a prompt template
-// GUIDANCE: Instructions to run this code: https://aka.ms/oai/net/start 
-#:package OpenAI@2.2.*-*
-#:property PublishAot false
+// GUIDANCE: Instructions to run this code: https://aka.ms/oai/net/start
+#pragma warning disable OPENAI001
+
+#:package OpenAI@2.*
+#:property PublishAot=false
 
 using OpenAI.Responses;
 using System.ClientModel;
