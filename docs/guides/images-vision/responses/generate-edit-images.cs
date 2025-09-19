9d7bef30c3aa3da3d4b69f3c9974c540f3f2603d
Justin Yoo <justin.yoojh@gmail.com>
2025-09-19 15:44:38 +0900

    Add image generation code sample

---
+++ docs/guides/images-vision/responses/generate-edit-images.cs
@@ -0,0 +1,34 @@
+// SAMPLE: Generate images with Responses API
+// PAGE: https://platform.openai.com/docs/guides/images-vision?api-mode=responses#generate-or-edit-images
+// GUIDANCE: Instructions to run this code: https://aka.ms/oai/net/start
+#pragma warning disable OPENAI001
+
+#:package OpenAI@2.*
+#:property PublishAot=false
+
+using System.ClientModel;
+using System.Text.Json;
+using OpenAI.Responses;
+
+string key = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
+OpenAIResponseClient client = new(model: "gpt-image-1", apiKey: key);
+
+OpenAIResponse response = (OpenAIResponse)client.CreateResponse(
+        BinaryContent.Create(BinaryData.FromObjectAsJson(new
+        {
+            model = "gpt-4.1",
+            input = "Generate an image of gray tabby cat hugging an otter with an orange scarf",
+            tools = new[]
+            {
+                new
+                {
+                    type = "image_generation"
+                }
+            }
+        }
+    ))
+);
+
+var serialised = JsonSerializer.Serialize(response.OutputItems);
+
+Console.WriteLine(serialised);
