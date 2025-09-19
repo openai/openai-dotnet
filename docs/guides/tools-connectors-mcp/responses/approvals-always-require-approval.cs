386ed2817eaa616cf7052e00e6411d4a22883c62
Justin Yoo <justin.yoojh@gmail.com>
2025-09-19 14:06:00 +0900

    Update code sample for always require approval

--- docs/guides/tools-connectors-mcp/responses/approvals-always-require-approval.cs
+++ docs/guides/tools-connectors-mcp/responses/approvals-always-require-approval.cs
@@ -11,34 +11,26 @@ using OpenAI.Responses;
 string key = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
 OpenAIResponseClient client = new(model: "gpt-5", apiKey: key);
 
-ResponseCreationOptions options1 = new();
-options1.Tools.Add(ResponseTool.CreateMcpTool(
-    serverLabel: "dmcp",
-    serverUri: new Uri("https://dmcp-server.deno.dev/sse")
-));
-
-McpToolCallApprovalRequestItem request1 = ResponseItem.CreateMcpApprovalRequestItem(
-    serverLabel: "dmcp",
-    name: "roll",
-    arguments: BinaryData.FromObjectAsJson(new { diceRollExpression = "2d4+1" })
-);
-
-OpenAIResponse response1 = (OpenAIResponse)client.CreateResponse([
-    request1
-], options1);
-
-ResponseCreationOptions options2 = new();
-options2.Tools.Add(ResponseTool.CreateMcpTool(
+ResponseCreationOptions options = new();
+options.Tools.Add(ResponseTool.CreateMcpTool(
     serverLabel: "dmcp",
     serverUri: new Uri("https://dmcp-server.deno.dev/sse"),
     toolCallApprovalPolicy: new McpToolCallApprovalPolicy(GlobalMcpToolCallApprovalPolicy.AlwaysRequireApproval)
 ));
 
-options2.PreviousResponseId = response1.Id;
+// STEP 1: Create response that requests tool call approval
+OpenAIResponse response1 = (OpenAIResponse)client.CreateResponse([
+    ResponseItem.CreateUserMessageItem([
+        ResponseContentPart.CreateInputTextPart("Roll 2d4+1")
+    ])
+], options);
 
+McpToolCallApprovalRequestItem? approvalRequestItem = response1.OutputItems.Last() as McpToolCallApprovalRequestItem;
+
+// STEP 2: Approve the tool call request and get final response
+options.PreviousResponseId = response1.Id;
 OpenAIResponse response2 = (OpenAIResponse)client.CreateResponse([
-    request1,
-    ResponseItem.CreateMcpApprovalResponseItem(request1.Id, approved: true),
-], options2);
+    ResponseItem.CreateMcpApprovalResponseItem(approvalRequestItem!.Id, approved: true),
+], options);
 
 Console.WriteLine(response2.GetOutputText());
