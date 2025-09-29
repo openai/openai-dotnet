837d94f5b914c8a93b04e14e21a436042a6d8b64
GitHub Action <action@github.com>
2025-09-29 20:52:59 +0000

    Update @typespec/http-client-csharp to 1.0.0-alpha.20250929.2
    - Updated @typespec/http-client-csharp from 1.0.0-alpha.20250926.1 to 1.0.0-alpha.20250929.2
    - Updated Microsoft.TypeSpec.Generator.ClientModel from 1.0.0-alpha.20250926.1 to 1.0.0-alpha.20250929.2
    - Regenerated OpenAI SDK code with new generator version
    - Updated centrally managed package-lock.json file with new dependency versions

--- src/Generated/Models/Audio/InternalCreateTranscriptionResponseVerboseJson.Serialization.cs
+++ src/Generated/Models/Audio/InternalCreateTranscriptionResponseVerboseJson.Serialization.cs
@@ -3,6 +3,7 @@
 #nullable disable
 
 using System;
+using System.ClientModel;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -215,5 +216,12 @@ namespace OpenAI.Audio
         }
 
         string IPersistableModel<InternalCreateTranscriptionResponseVerboseJson>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
+
+        public static explicit operator InternalCreateTranscriptionResponseVerboseJson(ClientResult result)
+        {
+            using PipelineResponse response = result.GetRawResponse();
+            using JsonDocument document = JsonDocument.Parse(response.Content);
+            return DeserializeInternalCreateTranscriptionResponseVerboseJson(document.RootElement, ModelSerializationExtensions.WireOptions);
+        }
     }
 }
