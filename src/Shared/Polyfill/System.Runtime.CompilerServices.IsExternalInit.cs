fef9a4035feebbaffacb3983e2f0b6df0c551c90
Christopher Scott <chriscott@hotmail.com>
2026-02-03 12:16:50 -0600

    remove Shared project

--- src/Shared/Polyfill/System.Runtime.CompilerServices.IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-#if !NET5_0_OR_GREATER
-
-using System.ComponentModel;
-namespace System.Runtime.CompilerServices;
-
-[EditorBrowsable(EditorBrowsableState.Never)]
-internal static class IsExternalInit { }
-
-#endif // !NET5_0_OR_GREATER
