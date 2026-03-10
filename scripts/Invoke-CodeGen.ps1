d83a339e619edfface1c4789753209388c6866df
Christopher Scott <chriscott@hotmail.com>
2026-03-10 18:30:25 -0500

    WIP

--- scripts/Invoke-CodeGen.ps1
+++ scripts/Invoke-CodeGen.ps1
@@ -316,8 +316,8 @@ try {
     Write-ElapsedTime "npm run build complete"
 
     Set-Location $specificationFolderPath
-    Invoke-ScriptWithLogging { npx tsp compile ./main_openai.tsp --options "@open-ai/plugin.emitter-output-dir={project-root}/.." --stats --trace @typespec/http-client-csharp }
-    Invoke-ScriptWithLogging { npx tsp compile ./main_responses.tsp --options "@open-ai/plugin.emitter-output-dir={project-root}/../Responses" --options "@open-ai/plugin.package-name=OpenAI.Responses" --stats --trace @typespec/http-client-csharp }
+    Invoke-ScriptWithLogging { npx tsp compile ./main_openai.tsp --options "@open-ai/plugin.emitter-output-dir={project-root}/../OpenAI" --stats --trace @typespec/http-client-csharp }
+    # Invoke-ScriptWithLogging { npx tsp compile ./main_responses.tsp --options "@open-ai/plugin.emitter-output-dir={project-root}/../Responses" --options "@open-ai/plugin.package-name=OpenAI.Responses" --stats --trace @typespec/http-client-csharp }
 }
 finally {
     Pop-Location
