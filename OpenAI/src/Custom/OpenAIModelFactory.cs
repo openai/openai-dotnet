5006d446918993e8ce6d719f48cae123dab65b17
LazyLinh2 <linh.phan3413@gmail.com>
2026-08-05 10:26:05 -0700

    Fix generated file error

--- OpenAI/src/Custom/OpenAIModelFactory.cs
+++ OpenAI/src/Custom/OpenAIModelFactory.cs
@@ -1,8 +1,14 @@
 using Microsoft.TypeSpec.Generator.Customizations;
+using OpenAI.Assistants;
 
 namespace OpenAI;
 
+[CodeGenSuppress("RunStepFileSearchResultContent", typeof(string))]
 [CodeGenType("OpenAIModelFactory")]
 internal static partial class OpenAIModelFactory
 {
+	public static RunStepFileSearchResultContent RunStepFileSearchResultContent(string text = default)
+	{
+		return new RunStepFileSearchResultContent(RunStepFileSearchResultContentKind.Text, text, additionalBinaryDataProperties: null);
+	}
 }
