d6a1ccd16aeda40120deb936c2c1c3f481dfe046
LazyLinh2 <linh.phan3413@gmail.com>
2026-08-31 14:56:41 -0700

    fix potential file handle leak, update samples

--- docs/guides/streaming-responses/responses/enable_streaming.cs
+++ docs/guides/streaming-responses/responses/enable_streaming.cs
@@ -2,6 +2,7 @@
 // PAGE: https://platform.openai.com/docs/guides/streaming-responses#enable-streaming
 // GUIDANCE: Instructions to run this code: https://aka.ms/oai/net/start
 #pragma warning disable OPENAI001
+#pragma warning disable SCME0005
 
 #:package System.Linq.Async@6.*
 #:package OpenAI@2.*
@@ -11,7 +12,7 @@ using OpenAI.Responses;
 string key = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
 ResponsesClient client = new(key);
 
-var responses = client.CreateResponseStreamingAsync(
+await using var responses = await client.CreateResponseStreamingAsync(
     "gpt-5.2",
     "Say 'double bubble bath' ten times fast."
 );
